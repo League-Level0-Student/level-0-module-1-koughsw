b92db9d0734d31e5bd5954d9d6fe0cc7bbadff13
league <yayalisa91@gmail.com>
2024-06-20 16:58:40 -0700

    Tasty Tomato WIP

---
+++ src/_03_if_else/_1_unbirthday/Unbirthday.java
@@ -0,0 +1,26 @@
+package _03_if_else._1_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+	public static void main(String[] args) {
+		
+	
+	String birthday = JOptionPane.showInputDialog("What is your birthday?");
+	if(birthday.equals ("06/20")) {
+		JOptionPane.showMessageDialog(null, "I wish you a happy birthday.");
+	}else {JOptionPane.showMessageDialog(null, "I wish you a very merry UNbirthday");
+	
+	
+	
+	
+	
+	}
+	
+	}
+	
+	
+	
+	
+	
+}
--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -6,13 +6,24 @@ import javax.swing.JOptionPane;
 
 public class Remarkable {
 public static void main(String[] args) {
-
+String carl = "You are very nice.";
+String james = "You are efficient at working.";
+String charles = "You are very good at playing video games.";
 		// 1. Save something remarkable about each person in a variable.
-
+	
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+String name = JOptionPane.showInputDialog("Enter a name."); 
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
-
+		if(name.equals("carl")) {
+			JOptionPane.showMessageDialog(null, carl);
+		}
+		if(name.equals("james")) {
+		JOptionPane.showMessageDialog(null, james);}
+		if(name.equals("charles")) {
+		JOptionPane.showMessageDialog(null, charles);
+		}else {
+		JOptionPane.showMessageDialog(null, "Who are you?");
+		
 	}
 }
-
+}
